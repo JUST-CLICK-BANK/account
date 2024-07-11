ea227e07861f89fa632ee4c672a02e5225f9725d
BoonDoPark <BoonDoPark>
2024-07-11 21:16:08 +0900

    [Feat] 계좌 생성 구현

--- src/main/java/com/click/account/service/AccountService.java
+++ src/main/java/com/click/account/service/AccountService.java
@@ -1,11 +1,9 @@
 package com.click.account.service;
 
 import com.click.account.domain.dto.request.AccountRequest;
-import com.click.account.domain.dto.request.GroupAccountRequest;
 
 import java.util.UUID;
 
 public interface AccountService {
     void saveAccount(UUID userId, AccountRequest req);
-    void saveGroupAccount(UUID userId, GroupAccountRequest req);
 }
---
+++ src/test/java/com/click/account/controller/AccountControllerTest.java
@@ -0,0 +1,104 @@
+package com.click.account.controller;
+
+import com.click.account.domain.dto.request.AccountRequest;
+import com.click.account.service.AccountService;
+import com.click.account.service.AccountServiceImpl;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
+@WebMvcTest(AccountController.class)
+class AccountControllerTest {
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @MockBean
+    AccountServiceImpl accountService;
+
+    @BeforeEach
+    void setUp() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
+
+    @Test
+    void 계좌_생성() throws Exception {
+        // given
+        UUID userId = UUID.fromString("71a90366-30e6-4e7e-a259-01a7947ff866");
+        AccountRequest request = new AccountRequest(
+                "0123",
+                "텅장"
+        );
+
+        String content = new ObjectMapper().writeValueAsString(request);
+
+        // when
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/accounts")
+                        .header("userId", userId.toString())
+                        .content(content)
+                        .contentType(MediaType.APPLICATION_JSON)
+                ).andDo(print());
+
+        // then
+        ArgumentCaptor<UUID> userIdCaptor = ArgumentCaptor.forClass(UUID.class);
+        ArgumentCaptor<AccountRequest> requestCaptor = ArgumentCaptor.forClass(AccountRequest.class);
+
+        Mockito.verify(accountService).saveAccount(userIdCaptor.capture(), requestCaptor.capture());
+
+        UUID capturedUserId = userIdCaptor.getValue();
+        AccountRequest capturedRequest = requestCaptor.getValue();
+
+        Assertions.assertEquals(userId, capturedUserId);
+        Assertions.assertEquals(request.accountName(), capturedRequest.accountName());
+    }
+
+    @Test
+    void 계좌_실패() throws Exception {
+        // given
+        UUID userId = UUID.fromString("71a90366-30e6-4e7e-a259-01a7947ff866");
+        AccountRequest request = new AccountRequest(
+                "0123",
+                "텅장"
+        );
+
+        String content = new ObjectMapper().writeValueAsString(request);
+
+        // when
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/accounts")
+                .header("userId", userId.toString())
+                .content(content)
+                .contentType(MediaType.APPLICATION_JSON)
+        ).andDo(print());
+
+        // then
+        ArgumentCaptor<UUID> userIdCaptor = ArgumentCaptor.forClass(UUID.class);
+        ArgumentCaptor<AccountRequest> requestCaptor = ArgumentCaptor.forClass(AccountRequest.class);
+
+        Mockito.verify(accountService).saveAccount(userIdCaptor.capture(), requestCaptor.capture());
+
+        UUID capturedUserId = userIdCaptor.getValue();
+        AccountRequest capturedRequest = requestCaptor.getValue();
+
+        Assertions.assertEquals(userId, capturedUserId);
+        Assertions.assertEquals(request.accountName(), capturedRequest.accountName());
+    }
+}
