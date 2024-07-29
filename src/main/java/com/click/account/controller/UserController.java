ae754e346e705c50411ed791803ed95b50a105e1
BoonDoPark <BoonDoPark>
2024-07-29 15:40:53 +0900

    [Fix] Dockerfile profile -> profiles로 변경

--- src/main/java/com/click/account/controller/UserController.java
+++ src/main/java/com/click/account/controller/UserController.java
@@ -3,18 +3,13 @@ package com.click.account.controller;
 import com.click.account.config.utils.jwt.JwtUtils;
 import com.click.account.config.utils.jwt.TokenInfo;
 import com.click.account.domain.dto.response.UserResponse;
-import com.click.account.domain.entity.User;
 import com.click.account.service.UserService;
-import java.util.UUID;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
