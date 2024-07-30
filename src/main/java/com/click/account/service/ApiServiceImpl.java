53939dcadc456b7ba14ede88d3b1071ced7b3d6c
BoonDoPark <76871728+BoonDoPark@users.noreply.github.com>
2024-07-30 18:01:48 +0900

    Feature/connect friend account (#49)
    * [Feat] 테스트 코드 및 로직 수정
    
    * [Fix] fix delete group member
    
    * [Fix] Dockerfile profile -> profiles로 변경
    
    * [Fix] password 로직 오류 수정
    
    * [Fix] 계좌 유저 조회 api 변경
    
    * [Fix] 기존 모임 통장 수락 대기 멤버 로직 수정
    
    * [Fix] 모임 통장 멤버 저장 수정
    
    * [Fix] 모임 통장 멤버 저장 로직 수정
    
    * 친구 테이블에 저장이 안되는 에러 발생
    
    ---------
    
    Co-authored-by: BoonDoPark <BoonDoPark>

--- src/main/java/com/click/account/service/ApiServiceImpl.java
+++ src/main/java/com/click/account/service/ApiServiceImpl.java
@@ -51,6 +51,7 @@ public class ApiServiceImpl implements ApiService {
                 .build();
             friends.add(friend);
         });
+        log.info(friends.toString());
 
         return friends;
     }
