cbe4f9f496ad9701e200466a26493ef9c0030e40
BoonDoPark <76871728+BoonDoPark@users.noreply.github.com>
2024-07-30 14:37:05 +0900

    Feature/connect friend account (#43)
    * [Feat] 테스트 코드 및 로직 수정
    
    * [Fix] fix delete group member
    
    * [Fix] Dockerfile profile -> profiles로 변경
    
    * [Fix] password 로직 오류 수정
    
    * [Fix] 계좌 유저 조회 api 변경
    
    ---------
    
    Co-authored-by: BoonDoPark <BoonDoPark>

--- src/main/java/com/click/account/controller/UserController.java
+++ src/main/java/com/click/account/controller/UserController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/users")
+@RequestMapping("/api/v1/accounts/users")
 @CrossOrigin(origins = "*", maxAge = 3600, allowedHeaders = "*",
     methods = {
         RequestMethod.GET,
